c06783a996730d6526de62804f877bb12df36731
A. Nesheret <a.nesheret@gmail.com>
2011-08-21 15:17:01 +0600

    Initial Commit

---
+++ Commons/Commons.h
@@ -0,0 +1,13 @@
+//
+//  Commons.h
+//  Commons
+//
+//  Created by Andrey Neshcheret on 21.08.11.
+//  Copyright 2011 InfiNet Wireless Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Commons : NSObject
+
+@end
---
+++ Commons/Commons.m
@@ -0,0 +1,23 @@
+//
+//  Commons.m
+//  Commons
+//
+//  Created by Andrey Neshcheret on 21.08.11.
+//  Copyright 2011 InfiNet Wireless Ltd. All rights reserved.
+//
+
+#import "Commons.h"
+
+@implementation Commons
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
